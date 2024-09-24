74dfc9e3e6048775946f9e656f99782ffd332174
Sr-meraj <marajmia3232@gmail.com>
2024-09-24 22:56:36 +0600

    Created a About Section

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,10 +7,10 @@ export default function Card({
   children,
 }: PropsWithChildren<{ className?: string }>) {
   return (
-    <div>
+    <>
       <div
         className={twMerge(
-          "bg-gray-800 rounded-3xl relative  z-0 after:z-10  after:content-[''] after:absolute after:inset-0 overflow-hidden after:outline after:outline-2 after:-outline-offset-2 after:rounded-3xl after:outline-white/20 after:pointer-events-none p-6 ",
+          "bg-gray-800 rounded-3xl relative  z-0 after:z-10  after:content-[''] after:absolute after:inset-0 overflow-hidden after:outline after:outline-2 after:-outline-offset-2 after:rounded-3xl after:outline-white/20 after:pointer-events-none",
           className
         )}
       >
@@ -22,6 +22,6 @@ export default function Card({
         ></div>
         {children}
       </div>
-    </div>
+    </>
   );
 }
--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,3 +1,175 @@
+import ChromeIcon from "@/assets/icons/chrome.svg";
+import CssIcon from "@/assets/icons/css3.svg";
+import GithubIcon from "@/assets/icons/github.svg";
+import HTMLIcon from "@/assets/icons/html5.svg";
+import ReactIcon from "@/assets/icons/react.svg";
+import JavascriptIcon from "@/assets/icons/square-js.svg";
+import bookImage from "@/assets/images/book-cover.png";
+import mapImage from "@/assets/images/map.png";
+import smileMemoji from "@/assets/images/memoji-smile.png";
+import Card from "@/components/Card";
+import CardHeader from "@/components/CardHeader";
+import SectionHeader from "@/components/SectionHeader";
+import ToolboxItems from "@/components/ToolboxItems";
+import Image from "next/image";
+
+const toolBoxItems = [
+  {
+    title: "JavaScript",
+    iconType: JavascriptIcon,
+  },
+  {
+    title: "HTML5",
+    iconType: HTMLIcon,
+  },
+  {
+    title: "CSS3",
+    iconType: CssIcon,
+  },
+  {
+    title: "React",
+    iconType: ReactIcon,
+  },
+  {
+    title: "Chrome",
+    iconType: ChromeIcon,
+  },
+  {
+    title: "Github",
+    iconType: GithubIcon,
+  },
+];
+
+const hobbies = [
+  {
+    title: "Painting",
+    emoji: "🎨",
+    left: "5%",
+    top: "5%",
+  },
+  {
+    title: "Photography",
+    emoji: "📸",
+    left: "50%",
+    top: "5%",
+  },
+  {
+    title: "Hiking",
+    emoji: "👟",
+    left: "35%",
+    top: "40%",
+  },
+  {
+    title: "Gaming",
+    emoji: "🎮",
+    left: "10%",
+    top: "35%",
+  },
+  {
+    title: "Music",
+    emoji: "🎵",
+    left: "70%",
+    top: "45%",
+  },
+  {
+    title: "Fitness",
+    emoji: "🏋️",
+    left: "5%",
+    top: "65%",
+  },
+  {
+    title: "Reading",
+    emoji: "📚",
+    left: "45%",
+    top: "70%",
+  },
+];
 export const AboutSection = () => {
-  return <div>About Section</div>;
+  return (
+    <div className="py-20 lg:py-28">
+      <div className="container">
+        <SectionHeader
+          eyebrow="About Me"
+          title="A Glimpse Into My World"
+          description="Learn more about who I am, What I do,"
+        />
+
+        <div className="mt-20 flex flex-col gap-8">
+          <div className="grid grid-cols-1  md:grid-cols-5 lg:grid-cols-3 gap-8">
+            <Card className="h-[320px] md:col-span-2 lg:col-span-1">
+              <CardHeader
+                title="My Reads"
+                description="Explore the books shaping my perspectives."
+              />
+              <div className="w-40 mx-auto mt-2 md:mt-0">
+                <Image
+                  src={bookImage}
+                  alt="Book Cover"
+                  className=""
+                />
+              </div>
+            </Card>
+            <Card className="h-[320px]  md:col-span-3 lg:col-span-2">
+              <CardHeader
+                title="My Toolbox"
+                description="Explore the technologies and tools I use to craft exceptional
+                digital experiences."
+                className=""
+              />
+              <ToolboxItems
+                items={toolBoxItems}
+                className=""
+              />
+              <ToolboxItems
+                items={toolBoxItems}
+                className="mt-6"
+                itemsWrapperclassName="-translate-x-1/2"
+              />
+            </Card>
+          </div>
+
+          <div className="grid grid-cols-1 md:grid-cols-5 lg:grid-cols-3 gap-8">
+            <Card className="h-[320px]  flex flex-col md:col-span-3 lg:col-span-2">
+              <CardHeader
+                title="Beyond the Code"
+                description="Explore my interests and hobbies beyond the digital realm."
+                className="px-6 pt-6"
+              />
+              <div className="relative flex-1">
+                {hobbies.map((hobby) => (
+                  <div
+                    key={hobby.title}
+                    className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute"
+                    style={{
+                      left: hobby.left,
+                      top: hobby.top,
+                    }}
+                  >
+                    <span className="font-meduim text-gray-950">
+                      {hobby.title}
+                    </span>
+                    <span>{hobby.emoji}</span>
+                  </div>
+                ))}
+              </div>
+            </Card>
+            <Card className="h-[320px] relative md:col-span-2 lg:col-span-1">
+              <Image
+                className="h-full w-full object-cover object-left-top"
+                src={mapImage}
+                alt="Map"
+              />
+              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-gray-950/30">
+                <Image
+                  className="size-20"
+                  src={smileMemoji}
+                  alt="smiling Memoji"
+                />
+              </div>
+            </Card>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
